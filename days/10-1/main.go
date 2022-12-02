b1059c513603f679f535345525dabc64ec41f4b2
Rob Ryan <rob@affclicks.com>
2022-12-03 09:26:25 +1100

    Day 10 part 2 + less convoluted part 1

--- days/10-1/main.go
+++ days/10-1/main.go
@@ -11,31 +11,32 @@ type position struct {
     y int
 }
 
+type angle struct {
+    x int
+    y int
+    positions []position
+}
+
 func main() {
     lines := readinput.ReadStrings("inputs/10/input.txt", "\n")
 
-    field := make([][]bool, len(lines))
     position_list := []position{}
 
     for y, line := range lines {
         positions := strings.Split(line, "")
-        field[y] = make([]bool, len(positions))
         for x, val := range positions {
             if val == "#" {
-                field[y][x] = true
                 position_list = append(position_list, position{
                     x,
                     y,
-                })
-            } else {
-                field[y][x] = false
-            }            
+                })   
+            }
         }
     }
 
     most_seen := 0
     for _, pos := range position_list {
-        seen := position_direction(field, position_list, pos.x, pos.y)
+        seen := position_direction(position_list, pos.x, pos.y)
         if seen > most_seen {
             most_seen = seen
         }
@@ -60,54 +61,32 @@ func max(a int, b int) int {
     return b
 }
 
-func position_direction(field [][]bool, position_list []position, x int, y int) int {
-    y_max := len(field)
-    x_max := len(field[0])
-
-    seen := len(position_list) - 1
+func position_direction(position_list []position, x int, y int) int {
+    seen := 0
 
     // calculate offsets
-    offset_list := []position{}
+    offset_base := map[int]map[int]bool{}
     for _, pos := range position_list {
         if pos.x != x || pos.y != y {
-            offset_list = append(offset_list, position{
-                x: pos.x - x,
-                y: pos.y - y,
-            })
-        }
-    }
+            offset_x := pos.x - x
+            offset_y := pos.y - y
 
-    removed := map[int]map[int]bool{}
-
-    for _, offset := range offset_list {
-        x_check := x + offset.x
-        y_check := y + offset.y
-
-        for i:=max(abs(offset.x), abs(offset.y)); i>0; i-- {
-            if offset.x % i == 0 && offset.y % i == 0 {
-                offset.x /= i
-                offset.y /= i
-                break
-            }
-        }
-
-        x_check += offset.x
-        y_check += offset.y
-
-        for x_check >= 0 && x_check < x_max && y_check >= 0 && y_check < y_max {
-            if field[y_check][x_check] {
-                if _, ok := removed[y_check][x_check]; !ok {
-                    if removed[y_check] == nil {
-                        removed[y_check] = make(map[int]bool)
-                    }
-
-                    removed[y_check][x_check] = true
-                    seen--
+            for i:=max(abs(offset_x), abs(offset_y)); i>0; i-- {
+                if offset_x % i == 0 && offset_y % i == 0 {
+                    offset_x /= i
+                    offset_y /= i
+                    break
                 }
             }
 
-            x_check += offset.x
-            y_check += offset.y
+            if offset_base[offset_y] == nil {
+                offset_base[offset_y] = make(map[int]bool)
+            }
+
+            if _, ok := offset_base[offset_y][offset_x]; !ok {
+                offset_base[offset_y][offset_x] = true
+                seen++
+            }
         }
     }
 
